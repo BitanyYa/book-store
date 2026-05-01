1c5ab347446c7131a3dfadbe5c3a3efc0391e96b
Bitaniya <bethanywondimagegn@gmail.com>
2026-05-01 22:19:10 +0300

    feat: initialize backend project and implement authentication module with JWT and bcrypt

--- server/backend/src/auth/auth.module.ts
+++ server/backend/src/auth/auth.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
 import { UsersModule } from '../users/users.module'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: 'super-secret-key', // we’ll move this to .env later
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- server/backend/src/auth/auth.service.ts
+++ server/backend/src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common'
 import { UsersService } from '../users/users.service'
 import * as bcrypt from 'bcrypt'
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async register(data: any) {
     return this.usersService.create(data)
@@ -23,9 +27,18 @@ export class AuthService {
       throw new UnauthorizedException('Invalid password')
     }
 
+    // create token payload
+    const payload = { sub: user.id, email: user.email }
+
+    const token = this.jwtService.sign(payload)
+
+    // remove password before returning
+    const { password, ...safeUser } = user
+
     return {
       message: 'Login successful',
-      user,
+      access_token: token,
+      user: safeUser,
     }
   }
 }
